7ad202a14b7a68e8362693bf59c44989ddacc5b6
Jamal Saleh <jamal.ngu@gmail.com>
2023-02-15 08:55:58 +0200

    quick starter

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+  return (
+    <>
+      <div className="header"></div>
+
+      <div className="footer"></div>
+    </>
+  );
+}
+export default Main;
---
+++ src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./SignUp.module.scss"
+function SignUp(){
+return (
+    <div className={styles.body}>
+        Hello world
+    </div>
+)
+}
+export default SignUp
